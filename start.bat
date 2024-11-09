0dbf588849ef0bc8c3b27f88740e9c13e1587b22
st184608 <st184608@stud.uni-stuttgart.de>
2024-11-09 18:21:30 +0100

    Start

--- start.bat
+++ start.bat
@@ -1,15 +1,14 @@
 @echo off
 
-REM Backend starten
+:: Backend starten (in einem neuen Fenster)
 echo Starting backend...
-start gradlew bootRun  REM Das Backend in einem neuen Fenster starten
+start cmd /k "gradlew bootRun"
 
-REM Warte kurz, um sicherzustellen, dass das Backend läuft
+:: Warte kurz, um sicherzustellen, dass das Backend läuft
 timeout /t 5
 
-REM Frontend starten
+:: Frontend starten (in einem neuen Fenster)
 echo Starting frontend...
-cd frontend  REM Wechsel ins Frontend-Verzeichnis
-npm run serve  REM Das Frontend starten
+start cmd /k "cd frontend && npm run serve"
 
-pause  REM Hält das Fenster offen
+pause
